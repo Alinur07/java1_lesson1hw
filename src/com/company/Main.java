34c548b9dad4fca9f0206314c0e1e1a76cf505b5
Alinur07 <arthuralimzhanov@gmail.com>
2022-05-06 07:14:08 +0300

    зделал дз

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        final int num = 13;
+        String word = "my age";
+        System.out.println(num + word);
+
+        if (num < 0)
+            System.out.println("Вы сохранили отрицательное число");
+        if (num > 0)
+            System.out.println("вы сохранили положительное число 'вы сохранили нуль");}}
+
+
+
+
+
+
+
+
